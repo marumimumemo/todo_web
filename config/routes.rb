51fdc8510c362b505988c983e11e59f02fa24681
丸本聡 <sagaminumber3@hotmail.co.jp>
2019-06-10 17:30:13 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root  'tasks#index'
-  resources :tasks, only: [:new, :create, :destroy]
+  resources :tasks, only: [:new, :create, :destroy, :edit, :update]
 end
