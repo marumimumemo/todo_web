275a4b85a7c538e037c7ba7aaaba46ad7cc43ea1
丸本聡 <sagaminumber3@hotmail.co.jp>
2019-06-11 21:54:29 +0900

    edit main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,12 +2,20 @@ $(document).on('turbolinks:load', function() {
 
   $(function() {
 
-    $('.switch').click(function(){
-        $('.finish').show();
+    $('.switch_on').click(function(){
+        $('.finished_tasklist').show();
     });
 
-    $('.switch').click(function(){
-        $('.unfinish').hide();
+    $('.switch_on').click(function(){
+        $('.unfinished_tasklist').hide();
+    });
+
+    $('.switch_off').click(function(){
+        $('.unfinished_tasklist').show();
+    });
+
+    $('.switch_off').click(function(){
+        $('.finished_tasklist').hide();
     });
 
   });
