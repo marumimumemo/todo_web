020922f2372a0e8a9ee23bdc7ab2dbe2db347c51
丸本聡 <sagaminumber3@hotmail.co.jp>
2019-06-17 21:38:23 +0900

    edit tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :body, :scheduled, :finished, :priority)
+    params.require(:task).permit(:title, :body, :scheduled, :finished, :priority, :unfinished)
   end
 
 end
